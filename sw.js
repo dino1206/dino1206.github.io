400e23c77099b815562561a80ff95d4a391838b2
GO BIG LUCK <dino1206@gmail.com>
2024-11-15 14:46:16 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -35,3 +35,16 @@ self.addEventListener('fetch', event => {
     })());
 });
 
+self.addEventListener('notificationclick', event => {
+    const notification = event.notification;
+    const action = event.action;
+    const link = notification.data.link;
+    if (action !== 'close') {
+        if (link) {
+            clients.openWindow(link);
+        }
+    }
+    notification.close();
+    console.log('notificationclick action is', action);
+})
+
