18c81ea614cc462b4f347c087b671b7fa1a1b1d6
GO BIG LUCK <dino1206@gmail.com>
2024-11-15 11:28:33 +0800

    1128

--- converter.js
+++ converter.js
@@ -40,3 +40,33 @@ form.addEventListener('input', () => {
     const outputTemp = convertTemp(inputTemp, fromUnit, toUnit);
     outputField.value = (Math.round(outputTemp * 100) / 100) + ' ' + toUnit.toUpperCase();
 });
+
+function askForNotificationPermission() {
+    Notification.requestPermission(function (result) {
+        // 這裡result只會有兩種結果：一個是用戶允許(granted)，另一個是用戶封鎖(denied)
+        console.log('User Choice', result);
+        if (result !== 'granted') {
+            console.log('No notification permission granted!');
+        } else {
+            displayConfirmNotification();
+        }
+    });
+}
+
+if ('Notification' in window) {
+    for (var i = 0; i < enableNotificationButtons.length; i++) {
+        enableNotificationButtons[i].style.display = 'inline-block';
+        enableNotificationButtons[i].addEventListener('click', askForNotificationPermission);
+    }
+}
+
+function displayConfirmNotification() {
+    if ('serviceWorker' in navigator) {
+        var options = {
+            body: '您已成功訂閱我們的推播服務!'
+        }
+        navigator.serviceWorker.ready.then(function (swreg) {
+            swreg.showNotification('成功訂閱!! (from Service Worker)', options);
+        });
+    }
+}
