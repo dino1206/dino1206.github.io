bef4d6d7fa3fefdb8aad17f3c52ea6b99fdd36c4
GO BIG LUCK <dino1206@gmail.com>
2024-11-18 15:27:38 +0800

    secondtry

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,28 @@
+importScripts('https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/11.0.2/firebase-messaging.js');
+
+// Initialize the Firebase app in the service worker by passing in the messagingSenderId
+firebase.initializeApp({
+	  apiKey: "AIzaSyC6aaGbABDxCwGDvEnR2dsKFog2lgekshI",
+	  authDomain: "pwa114.firebaseapp.com",
+	  projectId: "pwa114",
+	  storageBucket: "pwa114.firebasestorage.app",
+	  messagingSenderId: "665617311410",
+	  appId: "1:665617311410:web:bbc9f3a25204c0b9338bab",
+	  measurementId: "G-7TG5F5SRZV"
+});
+
+// Retrieve an instance of Firebase Messaging so that it can handle background messages.
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  // Customize notification here
+  const notificationTitle = 'Background Message Title';
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/firebase-logo.png'
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
