585712daf6e896439a26cd4a7d1127d9322c75cf
GO BIG LUCK <dino1206@gmail.com>
2024-11-18 15:30:15 +0800

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,15 +1,16 @@
-importScripts('https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/11.0.2/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/11.0.3/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/11.0.3/firebase-messaging.js');
+
 
 // Initialize the Firebase app in the service worker by passing in the messagingSenderId
 firebase.initializeApp({
-	  apiKey: "AIzaSyC6aaGbABDxCwGDvEnR2dsKFog2lgekshI",
-	  authDomain: "pwa114.firebaseapp.com",
-	  projectId: "pwa114",
-	  storageBucket: "pwa114.firebasestorage.app",
-	  messagingSenderId: "665617311410",
-	  appId: "1:665617311410:web:bbc9f3a25204c0b9338bab",
-	  measurementId: "G-7TG5F5SRZV"
+  apiKey: "AIzaSyC6aaGbABDxCwGDvEnR2dsKFog2lgekshI",
+  authDomain: "pwa114.firebaseapp.com",
+  projectId: "pwa114",
+  storageBucket: "pwa114.firebasestorage.app",
+  messagingSenderId: "665617311410",
+  appId: "1:665617311410:web:bbc9f3a25204c0b9338bab",
+  measurementId: "G-7TG5F5SRZV"
 });
 
 // Retrieve an instance of Firebase Messaging so that it can handle background messages.
